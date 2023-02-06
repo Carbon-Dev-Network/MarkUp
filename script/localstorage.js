a029620c7fd0f9d066fd3070f2df8454b07b2d4b
CleverCarpet <austinjdub@outlook.com>
2023-02-06 14:17:48 -0700

    Fix

--- script/localstorage.js
+++ script/localstorage.js
@@ -14,7 +14,7 @@ if (editorsList != undefined) {
 }
 
 function saveFile(file, filename, content) {
-    if (editors.editors.find(e => e.file === file + ',' + filename)) {
+    if (editors.editors.find(e => e.file === file + ',' + file)) {
         localStorage.setItem(file + ',' + filename, content)
         return 'Success'
     } else {
@@ -35,4 +35,8 @@ function readFile(file) {
 
 if (window.location.pathname == '/editor') {
     mddocument = readFile(editors.editors.find(e => e.file === window.location.hash.replace('#', '')).file)
+} else if (window.location.pathname == '/') {
+    editors.editors.forEach(function (item) {
+        document.getElementById('cardsDiv').innerHTML = document.getElementById('cardsDiv').innerHTML + `<a class="ui centered raised link card" href="/editor#` + item.file + `"> <div class="content"> <div class="header">`+item.file+`</div> <div class="meta">README.md</div> <div class="description"> An Markdown file </div> </div> <div class="ui buttons bottom attached button"> <button class="ui button">Edit</button> <button class="ui button" style="max-width:40%">Remove</button> </div> </a>`
+    })
 }
